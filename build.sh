09d07fd564834baf912c75302b95f6628edb9855
Nick Tyler <tylerns@email.sc.edu>
2019-08-28 21:45:03 +0000

    Push docker images after build

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 DOCKER_FILES=$(find build -type d -print | cut -d'/' -f2 | grep -v build | sort)
+export DOCKER_BUILDKIT=1
+docker --version
 
 for name in ${DOCKER_FILES}; do
+    # Skip root because it takes too long to build on travis
     if [ "$name" == "root" ]; then
         continue;
     fi
-
+    echo -e "Building \e[32m${name} \e[39m"
     docker build -t uofscphysics/$name -f build/$name/Dockerfile build/$name/.;
+    docker push uofscphysics/$name;
 done;
