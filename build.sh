d9348aa19dfdacef18389e3c4f82c705f8591b0c
Nick Tyler <tylerns@email.sc.edu>
2019-08-28 21:18:23 +0000

    Get base to build first

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DOCKER_FILES=$(find build -type d -print | cut -d'/' -f2 | grep -v build)
+DOCKER_FILES=$(find build -type d -print | cut -d'/' -f2 | grep -v build | sort)
 
 for name in ${DOCKER_FILES}; do
     if [ "$name" == "root" ]; then
